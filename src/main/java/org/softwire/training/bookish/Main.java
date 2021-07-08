e4a40b68fb6ff454d48e01672960faf58578f4b9
ZOO\AreMel <arend.mellendijk@softwire.com>
2021-07-08 14:57:44 +0100

    Implemented jdbi

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -2,17 +2,19 @@ package org.softwire.training.bookish;
 
 import org.apache.tomcat.util.buf.StringCache;
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
 import java.sql.*;
+import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String database = "mydb";
+        String user = "root";
+        String password = "password";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -38,21 +40,23 @@ public class Main {
         } catch (SQLException e) {
             System.out.println("error occured");
         }
-
-
-
     }
 
     private static void jdbiMethod(String connectionString) {
         System.out.println("\nJDBI method...");
-
         // TODO: print out the details of all the books (using JDBI)
         // See this page for details: http://jdbi.org
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
 
+        List<Book> books = jdbi.withHandle(handle -> {
+            return handle.createQuery("SELECT * FROM Books")
+                    .mapToBean(Book.class)
+                    .list();
+        });
 
-
+        for(Book book: books)
+            System.out.println("bookids: " + String.valueOf(book.id_books) + " title: " + book.title + " author: " + book.author);
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,31 @@
 package org.softwire.training.bookish.models.database;
 
 public class Book {
+    public Integer id_books;
+    public String title;
+    public String author;
 
+    public Integer getId_books() {
+        return id_books;
+    }
+
+    public void setId_books(Integer id_books) {
+        this.id_books = id_books;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
